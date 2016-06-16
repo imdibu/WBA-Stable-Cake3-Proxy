ce291496bf3eff818ce133c17468919640a215cd
Adrian Eavaz <aeavaz@tremend.ro>
2016-06-16 18:53:03 +0300

    include unique hash into bulk order lines columns

--- src/Services/Models/BulkOrderLineProxy3.php
+++ src/Services/Models/BulkOrderLineProxy3.php
@@ -12,5 +12,34 @@ namespace App\Services\Models;
 class BulkOrderLineProxy3 extends BaseModelProxy3
 {
     public $model = 'BulkOrderLine';
-    public $foreign_key_alias = '';
+
+    private function unique_hash($key_line = null, $key_order = null)
+    {
+        $input = [
+            'line' => $key_line,
+            'order' => $key_order
+        ];
+
+        return md5(serialize($input));
+    }
+
+    public function create($model, $fields, $values)
+    {
+        $data = array_combine($fields, $values);
+
+        $fields[] = 'unique_hash';
+        $values[] = $this->unique_hash($data['line_no'], $data['order_id']);
+
+        return parent::create($model, $fields, $values);
+    }
+
+    public function update($model, $fields, $values)
+    {
+        $data = array_combine($fields, $values);
+
+        $fields[] = 'unique_hash';
+        $values[] = $this->unique_hash($data['line_no'], $data['order_id']);
+
+        return parent::update($model, $fields, $values);
+    }
 }
