e768c920ad84d824eba238bbaa1c650bfb5f174c
Adrian Eavaz <aeavaz@tremend.ro>
2016-06-23 18:03:32 +0300

    dispatch shell correctly

--- bin/proxy.php
+++ bin/proxy.php
@@ -14,8 +14,7 @@ if (!include($COATS2_ROOT . $DS . 'lib' . $DS . 'Cake' . $DS . 'Console' . $DS .
     $failed = true;
 }
 
-$binPath = dirname(dirname(__DIR__)) . $DS . 'bin' . $DS . 'cake.php';
-$appPath = dirname(dirname(__DIR__)) . $DS . 'legacy' . $DS . 'Project';
+$appPath = dirname(__DIR__) . $DS . 'legacy' . $DS . 'Project';
 
 $defaultArgs = [
     0 => __FILE__,
@@ -23,14 +22,9 @@ $defaultArgs = [
     2 => $appPath
 ];
 
-//always fails
-try {
 // bootstraping
-    $tmp = new CShellDispatcher();
-    unset($tmp);
-} catch (Exception $e) {
-    ;
-}
+$tmp = new CShellDispatcher($defaultArgs);
+unset($tmp);
 
 if (!empty($failed)) {
     trigger_error("CakePHP core could not be found.  Check the value of COATS2_CAKE_CORE_INCLUDE_PATH in COATS_APP/webroot/index.php.  It should point to the directory containing your " . $DS . "cake core directory and your " . $DS . "vendors root directory.", E_USER_ERROR);
